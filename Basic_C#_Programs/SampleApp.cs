220c2c14b02c505cdb9dbae574461a889633d1f1
marconelele <jimmy187bob@Gmail.com>
2023-12-20 20:48:50 +0000

    Escape Sequences

---
+++ Basic_C#_Programs/SampleApp.cs
@@ -0,0 +1,7 @@
+using System;
+
+class SampleApp {
+    static void Main() {
+        Console.WriteLine("Hello, World");
+    }
+}
---
+++ Basic_C#_Programs/myConsoleProject/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        string lineOne = "Welcome to Acme Accounting Systems";
+        string lineTwo = "Remember, we're \"accounting\" on you!";
+        Console.WriteLine(lineOne);
+        Console.WriteLine(lineTwo);
+
+        Console.ReadLine();
+        }
+    }
+
