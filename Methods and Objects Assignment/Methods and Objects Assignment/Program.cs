d94cd5f05a05df981e72c3b3cffc03667021dff5
Jakub Sobieraj <jimmy187bob@Gmail.com>
2025-05-07 11:58:58 +0100

    Methods and Objects Assignment
    Perform these actions and create a console app that includes the following:
    
    Create a class called Person and give it two properties, each of data type string. One called FirstName, the other LastName.
    
    Give this class a void method called SayName() that takes no parameters and simply writes the person's full name to the console in the format of: “Name: [full name]”.
    
    Create another class called Employee and have it inherit from the Person class. Give the Employee class a property called Id and have it be of data type int.
    
    Inside of the Main method, instantiate and initialize an Employee object with a first name of “Sample” and a last name of “Student”.
    
    Call the superclass method SayName() on the Employee object.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ Methods and Objects Assignment/Methods and Objects Assignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace InheritanceExample
+{
+    // This is the base class named Person
+    class Person
+    {
+        // Property to store the person's first name
+        public string FirstName { get; set; }
+
+        // Property to store the person's last name
+        public string LastName { get; set; }
+
+        // Method that writes the person's full name to the console
+        public void SayName()
+        {
+            // Concatenate first and last name and print it
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+
+    // This class Employee inherits from Person
+    class Employee : Person
+    {
+        // Property to store the employee's ID
+        public int Id { get; set; }
+    }
+
+    class Program
+    {
+        // Main method is the entry point of the application
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object and initialize its properties
+            Employee employee = new Employee
+            {
+                FirstName = "Sample", // Set FirstName
+                LastName = "Student", // Set LastName
+                Id = 12345            // Set Id (though not used in output)
+            };
+
+            // Call the SayName method, inherited from the Person class
+            employee.SayName();
+
+            // Wait for a key press before closing the console window
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
